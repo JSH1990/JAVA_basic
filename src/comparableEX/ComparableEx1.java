ef4e7009d7e71f0459a3fc48dba28ab6f3ef0113
DESKTOP-SN9D63J\JSH <t>
2024-06-12 11:22:07 +0900

    no message

--- src/comparableEX/ComparableEx1.java
+++ src/comparableEX/ComparableEx1.java
@@ -11,5 +11,26 @@ package comparableEX;
        같으면 0, 오른쪽이 크면 음수(-), 왼쪽이 크면 양수(+)
 
  */
-public class ComparableEx1 {
-}
+public class ComparableEx1
+
+/*
+@RequestParam String year
+
+필수입력 아니면 기본값을 줘야한다.
+required=false , defaultValue = "1", int year
+
+
+서버의 해당되는 매개변수를 안주면 400번대 에러
+
+@RequestParam String year 필수입력해야한다. 안주면 400번대 에러
+
+매개변수로 값을안주면 빈문자열로 들어가게 된다 " "
+
+@ExcetionHandler(Exception.class_
+public String catcher(Exception ex){
+return
+
+public String catcher(Exception ex)
+ex.printStackTrace();
+return "yoilError"
+ */
